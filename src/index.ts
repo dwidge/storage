0d959af02e2362a92452f163e839515a16cddff3
dwidge <dwidge+gh@gmail.com>
2023-07-16 05:01:12 +0200

    add package exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { createStorage } from "../lib/index";
-import { storageEnv } from "../lib/StorageEnv";
+import { createStorage } from "@dwidge/storage";
+import { storageEnv } from "@dwidge/storage/StorageEnv";
 
 const storage = createStorage(storageEnv);
 storage.listAll("").then((list) => console.log("listAll1", list));
